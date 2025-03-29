d89398ef40a16991061c36c5031785efff1ccacc
ShahmeerAli <shahmeer.devel@gmail.com>
2025-03-29 05:24:41 +0500

    initial commit

---
+++ AppleOrangeTree.py
@@ -0,0 +1,29 @@
+
+
+
+a=4
+
+
+apples=[3,4,5,6,-2]
+
+newarray=[]
+
+s=7
+t=10
+
+
+for i in apples:
+    newarray.append(a+i)
+
+
+count=0
+
+for i in newarray:
+    if i >s and i<t:
+        count=count+1
+
+
+
+
+
+print(count)
